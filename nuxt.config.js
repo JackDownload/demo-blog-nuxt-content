d09a03a0ce5eff431930cebd6faeb5b8427a4012
JackDownload <kismocsy@gmail.com>
2021-09-03 14:08:45 +0200

    cv

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,55 +19,81 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap' },],
+    script: [
+        { src: 'https://polyfill.io/v3/polyfill.min.js?features=es6', body: true },
+      ]
   },
   /*
    ** Global CSS
    */
-  css: [],
-  /*
-   ** Plugins to load before mounting the App
-   ** https://nuxtjs.org/guide/plugins
-   */
-  plugins: [],
-  /*
-   ** Auto import components
-   ** See https://nuxtjs.org/api/configuration-components
-   */
-  components: true,
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/stylelint-module
-    '@nuxtjs/stylelint-module',
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss'
+   css: [
+    '@/assets/scss/style.scss',
   ],
-  /*
-   ** Nuxt.js modules
-   */
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    { src: '~/plugins/bootstrap.client.js' }
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // Doc: https://github.com/nuxt/content
-    '@nuxt/content'
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+    'bootstrap-vue/nuxt',
+    '@nuxtjs/style-resources',
+    '@nuxtjs/sitemap',
+    '@nuxtjs/gtm',
   ],
   /*
    ** Content module configuration
    ** See https://content.nuxtjs.org/configuration
    */
-  content: {
-    markdown: {
-      prism: {
-        theme: 'prism-themes/themes/prism-material-oceanic.css'
-      }
-    },
-    nestedProperties: ['author.name']
+   pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Content module configuration: https://go.nuxtjs.dev/config-content
+  content: {},
+  sitemap: {
+    hostname: 'https://josies-recipes.netlify.app/',
+    gzip: true,
+    routes: async () => {
+      const { $content } = require('@nuxt/content')
+      const articles = await $content('articles').only(['path']).fetch()
+      return articles.map((p) => p.path)
+    }
   },
   /*
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
    */
-  build: {}
+   build: {
+    loaders: {
+      vue: {
+        compilerOptions: {
+          whitespace: 'condense',
+          preserveWhitespace: false
+        }
+      },
+      sass: {
+        implementation: require('sass')
+      },
+      scss: {
+        implementation: require('sass')
+      }
+    }
+  }
 }
